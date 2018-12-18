1ecba32a5dd19c8ee2af65e159d5b586f0e91497
angelinn <angelin.nedelchev@outlook.com>
2018-12-18 14:01:58 +0200

    Add vote window/vm.

---
+++ src/ScheduledVoter/Voter.ViewModels/VoteViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voter.ViewModels
+{
+    public class VoteViewModel : BaseViewModel
+    {
+        private const int REQUEST_DELAY_MS = 5000;
+
+        public async Task CheckVotesAsync()
+        {
+            
+        }
+
+        public async Task VoteAsync()
+        {
+            foreach (string voteUrl in configurationService.Configuration.Votes)
+            {
+                await httpClient.GetAsync(voteUrl);
+                await Task.Delay(REQUEST_DELAY_MS);
+            }
+        }
+    }
+}
